cbe1663be629207dcdfd8e239273a6d964eb2e88
Lee Seung Yeap <homelsy1@naver.com>
2022-09-24 00:09:12 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# 파이썬 버전 3.7 이상 설치
+# pip install fastapi
+# pip install uvicorn[standard]" //실시간 미리보기
+# uvicorn main:app --reload
+# pip list 
+from fastapi import FastAPI
+app = FastAPI()
+
+@app.get("/")
+def test():
+    return 'test'
+
